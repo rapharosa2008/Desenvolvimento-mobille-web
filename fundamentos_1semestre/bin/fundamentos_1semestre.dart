ab52e1efdd555970b63238b4a3a783deb07488e4
Raphael Rosa <raphafamilia10000@gmail.com>
2026-04-02 22:44:49 +0000

    Criação do Projeto Dart

---
+++ fundamentos_1semestre/bin/fundamentos_1semestre.dart
@@ -0,0 +1,5 @@
+import 'package:fundamentos_1semestre/fundamentos_1semestre.dart' as fundamentos_1semestre;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fundamentos_1semestre.calculate()}!');
+}
---
+++ fundamentos_1semestre/test/fundamentos_1semestre_test.dart
@@ -0,0 +1,8 @@
+import 'package:fundamentos_1semestre/fundamentos_1semestre.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
